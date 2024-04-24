9cc489dbb771c317a5dd66e421fea7e82a57d3b0
Mnichols <malcolm_nichols@yahoo.com>
2024-04-24 09:11:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
+
+@app.get("/echo")
+def echo(message: Optional[str] = None):
+    if message:
+        return {"message": message}
+    return {"message": "No message provided"}
