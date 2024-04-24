8eaac798f51557c33400078d8fed59c1d57a5a6b
Mike Silva <mike.a.silva@gmail.com>
2024-04-24 10:40:33 -0400

    Added uvicorn

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 class Message(BaseModel):
     message: str
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
@@ -18,3 +18,9 @@ class Message(BaseModel):
 @app.post("/echo")
 def echo(message: Message):
     return {"message": message.message}
+
+'''
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+'''
