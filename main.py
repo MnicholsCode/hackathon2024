6d017df9689ac4dedd66f52b68f2bbe3ef300f0c
Mnichols <malcolm_nichols@yahoo.com>
2024-04-24 10:11:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Message(BaseModel):
+    message: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
 
-# @app.get("/echo")
-# def echo(message: Optional[str] = None):
-#     if message:
-#         return {"message": message}
-#     return {"message": "No message provided"}
+@app.post("/echo")
+def echo(message: Message):
+    return {"message": message.message}
