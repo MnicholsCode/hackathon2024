b6a553b77b0721c589a2673537f0b802bc4c7115
Malcolm <malcolm_nichols@yahoo.com>
2024-04-24 16:12:04 -0400

    Added submission date and as of date for status retrieval

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 csv_file = 'data.csv'
@@ -15,13 +16,27 @@ async def root():
 @app.get("/status/{application_id}")
 async def get_application_status(application_id: int):
     try:
+        # Load data into python
         df = pd.read_csv(csv_file)
+        # Find the match on the application id
         result = df[df['application_id'] == application_id]
+        # Get submission date from application
+        submission_date = result['submission_date'].iloc[0]
+        # Get our as_of_date
+        as_of_date = datetime.now().strftime("%m%d%Y")
+        # Get applications status
+        status = result['status'].iloc[0]
+        # Check if the id is empty
         if result.empty:
             return f"The Application id for {application_id} is incorrect or does not exist, please re-enter the id or consult..."
-        return f"The Application status for: {application_id} is {result['status'].iloc[0]}"
+        
+        # Setup output string for bot
+        output_string = f"As of {as_of_date}. The applications status for ID:{application_id} is {status}, which was submitted on {submission_date}"
+        return output_string
+
     except FileNotFoundError:
         return "Data file does not exist."
+    
     except Exception as e:
         return {"error": str(e)}
 
