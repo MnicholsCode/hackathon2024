d069d9f407d67f1bc1b17561e55ebf1a9bfb3a27
Malcolm <malcolm_nichols@yahoo.com>
2024-04-25 17:15:02 -0400

    Final bug fix.....maybe

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ async def update_application(update_data: ApplicationUpdate, db: Session = Depen
 # Display all applications depending on their status    
 @app.get("/applications/status/", response_model=List[ApplicationResponse])
 async def get_applications_by_status(status: str = Query(..., enum=["Pending", "Reviewed", "Completed"]), db: Session = Depends(get_db)):
-    applications = db.query(ApplicationDB).filter(ApplicationDB.status == status).all()
+    applications = db.query(ApplicationDB).filter(func.lower(ApplicationDB.status) == status.lower()).all()
     if not applications:
         raise HTTPException(status_code=404, detail="No applications found with the specified status")
     return applications
